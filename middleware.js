b554557ebde1876f7ffaf10c092eab7f0f9ef05d
张伟杰 <email@zhangweijie.cn>
2016-02-16 14:42:08 +0800

    Optimize the code structure

--- middleware.js
+++ middleware.js
@@ -1,45 +1,46 @@
 var http = require('http');
 
-exports.createServer = function () {
-  var app = {}
-  var middlewares = [];
+var app = {};
 
-  var server = http.createServer(function (req, res) {
-    function next(index) {
-      if(index == undefined) {
-        index = 0;
-      }
-      return function () {
-        if(middlewares[index].path == '/' || middlewares[index].path == req.url) {
-          middlewares[index].handle(req, res, next(index + 1));
-          return;
-        } else {
-          next(index + 1)();
-        }
+app.handle = function (req, res) {
+  function next(index) {
+    if(index == undefined) {
+      index = 0;
+    }
+    return function () {
+      if(app.middlewares[index].path == '/' || app.middlewares[index].path == req.url) {
+        app.middlewares[index].handle(req, res, next(index + 1));
+        return;
+      } else {
+        next(index + 1)();
       }
     }
-    next()();
+  }
+  next()();
+}
+
+app.use = function (path, handle) {
+  if('string' !== typeof path) {
+    handle = path;
+    path = '/';
+  }
+
+  if('function' !== typeof handle) {
+    throw new Error('Arguments type error');
+  }
+
+  this.middlewares.push({
+    path: path,
+    handle: handle
   });
+}
 
-  app.use = function (path, handle) {
-    if('string' !== typeof path) {
-      handle = path;
-      path = '/';
-    }
+app.listen = function() {
+  return this.server.listen.apply(this.server, arguments)
+}
 
-    if('function' !== typeof handle) {
-      throw new Error('Arguments type error');
-    }
-
-    middlewares.push({
-      path: path,
-      handle: handle
-    });
-  }
-
-  app.listen = function() {
-    return server.listen.apply(server, arguments)
-  }
-
+exports.createServer = function () {
+  app.middlewares = [];
+  app.server = http.createServer(app.handle);
   return app;
 }
