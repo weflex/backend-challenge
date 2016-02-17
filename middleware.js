f8930e2e785f853197e8833bd7a1b08ee4fe0faf
Zhang Weijie <email@zhangweijie.cn>
2016-02-18 03:30:40 +0800

    Fix the code style with JavaScript standard

--- middleware.js
+++ middleware.js
@@ -5,40 +5,44 @@ var app = {};
 app.handle = function (req, res) {
   var index = 0;
   function next() {
-    if(index >= app.middlewares.length) {
+    if (index >= app.middlewares.length) {
       return;
     }
+
     var middleware = app.middlewares[index++];
-    if(middleware.path === '/' || middleware.path === req.url) {
+    if (middleware.path === '/' || middleware.path === req.url) {
       middleware.handle(req, res, next);
       return;
     } else {
       next();
     }
   }
+
   next();
-}
+};
 
 app.use = function (path, handle) {
-  if('string' !== typeof path) {
+  if (typeof path !== 'string') {
     handle = path;
     path = '/';
   }
-  if('function' !== typeof handle) {
+
+  if (typeof handle !== 'function') {
     throw new TypeError('handle is required to be a function.');
   }
+
   this.middlewares.push({
     path: path,
-    handle: handle
+    handle: handle,
   });
-}
+};
 
-app.listen = function() {
-  return this.server.listen.apply(this.server, arguments)
-}
+app.listen = function () {
+  return this.server.listen.apply(this.server, arguments);
+};
 
 exports.createServer = function () {
   app.middlewares = [];
   app.server = http.createServer(app.handle);
   return app;
-}
+};
