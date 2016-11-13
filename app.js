06a83db7f57adec858bda4e844ece21ece0a3fef
yooki0328 <wcx970328@gmail.com>
2016-11-13 16:24:32 +0800

    Yooki

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 var middleware = require('./middleware');
-
 var app = middleware.createServer();
+//var app2 = middleware.createServer()
+//app2.use('/bb',function(req,res,next){
+    console.log('bb')
+    next()
+})
 
 app.use(function (req, res, next) {
+  //console.log(req.url)
   res.write('preamble');
   next();
 });
-
+//app.use('/aa',app2)
 app.use('/hello', function (req, res, next) {
   res.write('hello');
   next();
 });
 
 app.use('/world', function (req, res, next) {
-  res.write('world');
+console.log('/world\'s url'+req.url)
+res.write('world');
+  console.log('asd')
   next();
 });
 
@@ -22,4 +29,5 @@ app.use('/', function (req, res, next) {
   console.log('END');
 });
 
-app.listen(3000);
+app.listen(3000,()=>{
+    console.log('server+3000')});
